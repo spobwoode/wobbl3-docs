9fb677cd06a211ade7ca05fbc99408c63543d4c3
spobwoode <robinspots@gmail.com>
2023-03-16 16:59:14 +0000

    Added quick start guides and other pages

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Wobbl3 Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Wobbl3 Docs',
   },
 }
 
