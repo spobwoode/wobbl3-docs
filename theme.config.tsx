49d7c1778f7f657c6a4ec01bc9a691e75ce8de4b
spobwoode <robinspots@gmail.com>
2023-07-10 11:45:43 +0100

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
-import { DocsThemeConfig } from 'nextra-theme-docs';
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs';
+import { useRouter } from 'next/router';
 
 const config: DocsThemeConfig = {
   useNextSeoProps() {
@@ -18,18 +19,36 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'Wobbl3 Docs',
   },
-  head: (
-    <Fragment>
-      <meta name="msapplication-TileColor" content="#fff" />
-      <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="%s – wobbl3 Docs" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@shuding_" />
-      <meta property="og:title" content="%s – wobbl3 Docs" />
-      <meta property="og:description" content="%s – wobbl3 Docs" />
-      <meta name="apple-mobile-web-app-title" content="%s – wobbl3 Docs" />
-    </Fragment>
-  ),
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://docs.wobbl3.io' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'Wobbl3 Docs'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'Wobbl3 Docs'}
+        />
+      </>
+    )
+  }
+  // head: (
+  //   <Fragment>
+  //     <meta name="msapplication-TileColor" content="#fff" />
+  //     <meta httpEquiv="Content-Language" content="en" />
+  //     <meta name="description" content="%s – wobbl3 Docs" />
+  //     <meta name="twitter:card" content="summary_large_image" />
+  //     <meta name="twitter:site" content="@shuding_" />
+  //     <meta property="og:title" content="%s – wobbl3 Docs" />
+  //     <meta property="og:description" content="%s – wobbl3 Docs" />
+  //     <meta name="apple-mobile-web-app-title" content="%s – wobbl3 Docs" />
+  //   </Fragment>
+  // ),
 }
 
 export default config;
